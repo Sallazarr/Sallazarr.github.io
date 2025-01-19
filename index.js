758fc4cf53c19f14bbe62d580b78e0407bdc50ca
Sallazarr <henriquesalazards16@hotmail.com>
2025-01-19 12:43:50 -0300

    change lang

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const main = document.querySelector('main');
 const root = document.querySelector(':root');
+const html = document.querySelector('html')
 
-document.getElementById('switchTheme').addEventListener('click', function(){
+    document.getElementById('switchTheme').addEventListener('click', function(){
     if(main.dataset.theme === 'dark'){
         root.style.setProperty('--color-background', '#fff')
         root.style.setProperty('--color-white', '#030207')
@@ -14,5 +15,17 @@ document.getElementById('switchTheme').addEventListener('click', function(){
         root.style.setProperty('--color-blue', '#1474b4')
         root.style.setProperty('--color-hover', '#3e293a')
         main.dataset.theme = 'dark'   
+    }
+})
+
+document.getElementById('language').addEventListener('click', function(){
+    if(html.dataset.lang === 'ptbr'){
+        const theme = document.getElementById('switchTheme')
+        theme.innerHTML = 'Change theme light/dark'
+        html.dataset.lang = 'en'
+    } else {
+        const theme = document.getElementById('switchTheme')
+        theme.innerHTML = 'Trocar tema claro/escuro'
+        html.dataset.lang = 'ptbr'
     }
 })
